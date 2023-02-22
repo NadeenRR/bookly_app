2e0bf28c3f7644bc722080d387d28caa3720bf5d
Nadeen Radwan <nadeenradwan0@gmail.com>
2023-02-22 13:28:04 +0200

    adding sliding animation to splash view

--- lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,34 @@
 import 'package:bookly_app/core/utils/asset.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    slidingAimation = Tween<Offset>(begin: const Offset(0, 1), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +36,24 @@ class SplashViewBody extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset(
-            AssetData.logo,
-            width: MediaQuery.of(context).size.width / 1.5,
-          )
+          AnimatedBuilder(
+              animation: slidingAimation,
+              builder: (context, _) {
+                return SlideTransition(
+                  position: slidingAimation,
+                  child: Image.asset(
+                    AssetData.logo,
+                    width: MediaQuery.of(context).size.width / 1.7,
+                  ),
+                );
+              }),
+          // SizedBox(
+          //   height: MediaQuery.of(context).size.height / 35,
+          // ),
+          // const Text(
+          //   'Read Free Books',
+          //   style: TextStyle(fontSize: 15),
+          // )
         ],
       ),
     );
