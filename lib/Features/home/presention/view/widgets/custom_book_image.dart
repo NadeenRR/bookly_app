8a7ebd924ca4e78892b87657da4e91f9bfedfc75
Nadeen Radwan <nadeenradwan0@gmail.com>
2023-02-27 17:33:43 +0200

    using cached network image

--- lib/Features/home/presention/view/widgets/custom_book_image.dart
+++ lib/Features/home/presention/view/widgets/custom_book_image.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -10,14 +11,23 @@ class CustomBookImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.7 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.red,
-            image: DecorationImage(
-                fit: BoxFit.fill, image: NetworkImage(imageUrl))),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          // errorWidget: (context, url, error) => const Icon(Icons.error),
+        ),
+
+        // Container(
+        //   decoration: BoxDecoration(
+        //       borderRadius: BorderRadius.circular(16),
+        //       color: Colors.red,
+        //       image: DecorationImage(
+        //           fit: BoxFit.fill, image: NetworkImage(imageUrl))),
+        // ),
       ),
     );
   }
